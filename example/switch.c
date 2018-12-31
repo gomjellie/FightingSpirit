37522f92d07b38165597d0fa5c779c5b2d958134
gomjellie <gomjellie@gmail.com>
2019-01-01 02:25:32 +0900

    switch test case 작성

---
+++ example/switch.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n = 0;
+    scanf("%d", &n);
+
+    switch (n) {
+        case 1:
+            printf("hi");
+            break;
+        case 2:
+            printf("hello");
+            break;
+        default:
+            printf("hi");
+            break;
+    }
+
+    return 0;
+}
---
+++ example/switchBreakInOneLine.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n = 0;
+    scanf("%d", &n);
+
+    switch (n) {
+        case 1:
+            printf("hi"); break;
+        case 2:
+            printf("hello"); break;
+        default:
+            printf("hi\n\n"); break;
+    }
+
+    return 0;
+}
